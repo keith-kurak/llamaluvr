5bb300b3d9fb7dbbb6c1667ff8dfec7346956a8a
Keith Kurak <keith.kurak@gmail.com>
2026-05-12 11:14:52 -0400

    expo blogs

---
+++ content/expo-blogs.ts
@@ -0,0 +1,34 @@
+import type { LinkFile } from "@/components/content";
+
+export const EXPO_BLOGS: LinkFile[] = [
+  {
+    kind: "link",
+    id: "eas-update-best-practices",
+    title: "EAS Update best practices",
+    url: "https://expo.dev/blog/eas-update-best-practices",
+  },
+  {
+    kind: "link",
+    id: "why-expo-great-fit",
+    title: "Why Expo is a great fit for new and existing React Native apps",
+    url: "https://expo.dev/blog/why-expo-is-a-great-fit-for-new-and-existing-react-native-apps",
+  },
+  {
+    kind: "link",
+    id: "eas-workflows-github-actions",
+    title: "How to integrate EAS Workflows with GitHub Actions",
+    url: "https://expo.dev/blog/how-to-integrate-eas-workflows-with-github-actions",
+  },
+  {
+    kind: "link",
+    id: "incrementally-adopt-expo",
+    title: "How to incrementally adopt Expo",
+    url: "https://expo.dev/blog/how-to-incrementally-adopt-expo",
+  },
+  {
+    kind: "link",
+    id: "continuous-native-generation",
+    title: "What is Continuous Native Generation?",
+    url: "https://expo.dev/blog/what-is-continuous-native-generation",
+  },
+];
